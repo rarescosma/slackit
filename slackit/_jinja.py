3a9e828b21b73e2b11a6d7ca3771c7a4a191618a
Rareș Cosma <rares@getbetter.ro>
2020-10-17 12:51:19 +0200

    Remove unused code from jinja2 wrapper

--- slackit/_jinja.py
+++ slackit/_jinja.py
@@ -1,18 +1,7 @@
-"""Jinja templating directives."""
-from pathlib import Path
+"""Jinja2 wrapper."""
 from typing import Any, Dict
 
 import jinja2
-
-
-def render_jinja_template(
-    template_file: Path, context: Dict[str, Any], output_file: Path
-) -> None:
-    """Render the given jinja template file into the output file using
-    the context."""
-    output_file.write_text(
-        render_jinja(template=template_file.read_text(), context=context)
-    )
 
 
 def render_jinja(template: str, context: Dict[str, Any]) -> str:
